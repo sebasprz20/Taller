2de320747763fb6979001c1b79c85b239a312d1b
Estudiante <Estudiante@DESKTOP-19OJJPL>
2022-05-31 14:34:07 -0600

    123

--- index.aspx.cs
+++ index.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace logtest
 {
@@ -7,7 +9,86 @@ namespace logtest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["usuariologueado"] != null)
+            {
+                string usuariologueado = Session["usuariologueado"].ToString();
+                lblBienvenida.Text = "Bienvenido/a " + usuariologueado;
+            }
+            else
+            {
+                Response.Redirect("login.aspx");
+            }
 
+            lblError.Text = "";
+            lblErrorContrasenia.Text = "";
+            LeerDatos();
+        }
+        protected void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Session.Remove("usuariologueado");
+            Response.Redirect("login.aspx");
+        }
+        SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
+        void Limpiar()
+        {
+            tbNombres.Text = "";
+            tbApellidos.Text = "";
+            tbFechaNacimiento.Text = "";
+            tbUsuario.Text = "";
+            tbConfirmarContrasenia.Text = "";
+            tbContrasenia.Text = "";
+            lblError.Text = "";
+            lblErrorContrasenia.Text = "";
+        }
+        protected void BtnRegistrar_Click(Object sender, EventArgs e)
+        {
+            if (tbNombres.Text == "" || tbApellidos.Text == "" || tbFechaNacimiento.Text == "" || tbUsuario.Text == "" || tbConfirmarContrasenia.Text == "" || tbContrasenia.Text == "")
+            {
+                lblError.Text = "Ningun campo puede quedar vacío!";
+            }
+            else
+            {
+                if (tbContrasenia.Text != tbConfirmarContrasenia.Text)
+                {
+                    lblErrorContrasenia.Text = "Las contrasenias no coinciden!";
+                }
+                else
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("select count(*) from Usuarios where Usuario='" + tbUsuario.Text + "'", conexion)
+                    {
+                        CommandType = System.Data.CommandType.Text
+                    };
+                    cmd.Parameters.AddWithValue("Usuario", tbUsuario.Text);
+                    int usuario = Convert.ToInt32(cmd.ExecuteScalar());
+                    string patron = "xxsebasxx";
+                    if (usuario < 1)
+                    {
+                        SqlCommand cmm = new SqlCommand("Insert into Usuarios values('" + tbNombres.Text + "','" + tbApellidos.Text + "'," +
+                            "'" + tbFechaNacimiento.Text + "','" + tbUsuario.Text + "',(EncryptByPassPhrase('" + patron + "','" + tbContrasenia.Text + "')))", conexion);
+                        cmm.ExecuteNonQuery();
+                        conexion.Close();
+                        Limpiar();
+                        LeerDatos();
+                    }
+                    else
+                    {
+                        lblError.Text = "El Usuario " + tbUsuario.Text + " ya existe!";
+                        tbUsuario.Text = "";
+                    }
+                }
+            }
+        }
+
+        void LeerDatos()
+        {
+            SqlCommand leerdatos = new SqlCommand("Select * from Usuarios", conexion);
+            SqlDataAdapter da = new SqlDataAdapter(leerdatos);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvUsuarios.DataSource = dt;
+            gvUsuarios.DataBind();
         }
 
     }
--- index.aspx.designer.cs
+++ index.aspx.designer.cs
@@ -13,5 +13,176 @@ namespace logtest
 
     public partial class index
     {
+
+        /// <summary>
+        /// Control lblBienvenida.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBienvenida;
+
+        /// <summary>
+        /// Control formulario_registro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formulario_registro;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Control lblNombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombres;
+
+        /// <summary>
+        /// Control tbNombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNombres;
+
+        /// <summary>
+        /// Control lblApellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApellidos;
+
+        /// <summary>
+        /// Control tbApellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbApellidos;
+
+        /// <summary>
+        /// Control lblFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaNacimiento;
+
+        /// <summary>
+        /// Control tbFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbFechaNacimiento;
+
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+
+        /// <summary>
+        /// Control tbUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbUsuario;
+
+        /// <summary>
+        /// Control lblContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContrasenia;
+
+        /// <summary>
+        /// Control tbContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbContrasenia;
+
+        /// <summary>
+        /// Control lblConfirmarContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblConfirmarContrasenia;
+
+        /// <summary>
+        /// Control tbConfirmarContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbConfirmarContrasenia;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
+        /// Control lblErrorContrasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorContrasenia;
+
+        /// <summary>
+        /// Control BtnRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnRegistrar;
+
+        /// <summary>
+        /// Control gvUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataGrid gvUsuarios;
     }
 }
